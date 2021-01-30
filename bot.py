e48b71f8c6f6f125eec32b75ef576d77f4e8d9f0
Elias Hawa <elias.hawa2468@gmail.com>
2021-01-30 12:57:16 -0500

    Add str to event

--- bot.py
+++ bot.py
@@ -2,6 +2,10 @@ import discord
 from classes import Event
 from discord.ext import commands
 
+from classes.Event import Event
+
+ids = 0
+
 client = commands.Bot(command_prefix = '.')
 id_increment = 0
 events = []
@@ -19,8 +23,14 @@ async def ping(ctx):
 async def begin(ctx):
     global id_increment
     event = Event.Event(id_increment, "random name")
+    events.append(event)
     id_increment += 1
     await ctx.send(f'Someone started {event.eventName}. Use id {event.id} to enter queue.')
+
+@client.command(aliases=['listq', 'listqueue'])
+async def lq(ctx):
+    embedVar = discord.Embed(title="Test", description="")
+    await ctx.send(embed=embedVar)
 
 client.run('ODA1MTIwMTc4ODAyMzkzMTA4.YBWQmQ.HynCQfH1FcaRR-ah6UycFOd7sSs')
 
--- classes/Event.py
+++ classes/Event.py
@@ -1,6 +1,9 @@
 class Event:
-    def __init__(self, _id, _eventname):
+    def __init__(self, _id, _eventname, _host):
         self.id = _id
         self.eventName = _eventname
+        self.host = _host
         self.queue = []
 
+    def __str__(self):
+        return f'{self.eventName} hosted by: {self.host}. ID: {self.id}'

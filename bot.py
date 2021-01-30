a728c31191c1ad641a8eca634b95b82f1b1667dc
Elias Hawa <elias.hawa2468@gmail.com>
2021-01-30 12:17:47 -0500

    Add ping

--- bot.py
+++ bot.py
@@ -3,8 +3,13 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
 
-@client.event()
+@client.event
 async def on_ready():
     print('Bot is ready.')
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
 client.run('ODA1MTIwMTc4ODAyMzkzMTA4.YBWQmQ.HynCQfH1FcaRR-ah6UycFOd7sSs')
+

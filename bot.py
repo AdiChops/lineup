2bac3855563082fe6b73763f64a39ed4ff5f1d31
Elias Hawa <elias.hawa2468@gmail.com>
2021-01-30 12:13:42 -0500

    Add token

--- bot.py
+++ bot.py
@@ -6,3 +6,5 @@ client = commands.Bot(command_prefix = '.')
 @client.event()
 async def on_ready():
     print('Bot is ready.')
+
+client.run('ODA1MTIwMTc4ODAyMzkzMTA4.YBWQmQ.HynCQfH1FcaRR-ah6UycFOd7sSs')

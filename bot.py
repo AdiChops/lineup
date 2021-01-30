65327df0401beeb6ae78ec9aa8a97123c5b0d7f3
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2021-01-30 15:33:04 -0500

    begin flow

--- bot.py
+++ bot.py
@@ -2,13 +2,12 @@ import discord
 from classes import Event
 from discord.ext import commands
 
-from classes.Event import Event
-
 ids = 0
 
 client = commands.Bot(command_prefix = '.')
 id_increment = 0
 events = []
+
 
 @client.event
 async def on_ready():
@@ -19,18 +18,16 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
+
 @client.command()
-async def begin(ctx):
+async def begin(ctx, *, event_name):
     global id_increment
-    event = Event.Event(id_increment, "random name")
+    event = Event.Event(id_increment, event_name, ctx.author)
     events.append(event)
     id_increment += 1
-    await ctx.send(f'Someone started {event.eventName}. Use id {event.id} to enter queue.')
+    await ctx.send(f'{event.host} started {event.eventName}. Use id {event.id} to enter queue.')
 
-@client.command(aliases=['listq', 'listqueue'])
-async def lq(ctx):
-    embedVar = discord.Embed(title="Test", description="")
-    await ctx.send(embed=embedVar)
+
 
 client.run('ODA1MTIwMTc4ODAyMzkzMTA4.YBWQmQ.HynCQfH1FcaRR-ah6UycFOd7sSs')
 
